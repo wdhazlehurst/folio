deb87e85e9e54ad86a2fd0612d66fa85d7d692af
Will Hazlehurst <will@macbookpro.lan>
2025-06-19 23:16:09 -0400

    Authorization logic init

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,20 +1,74 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import { PrismaClient } from "../generated/prisma";
+import bcrypt from "bcrypt";
+import type { User } from "next-auth";
+
+
+const prisma = new PrismaClient();
 
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
+    // https://next-auth.js.org/providers/credentials
     Credentials({
       name: "Credentials",
       credentials: {
         email: { label: "Email", type: "text" },
         password: { label: "Password", type: "password" },
     },
-    async authorize(credentials) {
+
+    /**
+     * Authorization check against database credentials
+     * @param credentials Credentials containing email and password.
+     * @returns `null` if credentials are invalid, otherwise returns user object.
+     */
+    async authorize(credentials, request) {
       if (!credentials?.email || !credentials?.password) {
         return null;
       }
-    }
-  )
+
+      if (typeof credentials.email !== "string" || typeof credentials.password !== "string") {
+        return null;
+      }
+
+      const user = await prisma.user.findUnique({
+        where: { email: credentials.email },
+      });
+
+      if (!user) return null;
+
+      const isValid = await bcrypt.compare(credentials.password, user.password);
+      if (!isValid) return null;
+
+      // Any object returned will be saved in `user` property of JWT
+      return {
+        id: user.id,
+        email: user.email,
+      } as User;
+    },
+  }),
   ],
+
+  pages: {
+    signIn: "/login",
+  },
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+      }
+      return token;
+    },
+
+    async session({ session, token }) {
+      if (token && session.user) {
+        session.user.id = String(token.id);
+        session.user.email = String(token.email);
+      }
+      return session;
+    }
+  }
 })
---
+++ types/types.d.ts
@@ -0,0 +1,12 @@
+import 'next-auth';
+
+
+/**
+ * Extending the NextAuth User for custom implementation.
+ */
+declare module 'next-auth' {
+    export interface User {
+        id: integer;
+        email: string;
+    }
+}
