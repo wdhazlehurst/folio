2a54adcd89b3dd6aa375d0ca5fba879ac4a6f801
Will Hazlehurst <will@Wills-MacBook.local>
2025-09-01 23:31:56 -0400

    Fix for first render being server-side. Causes hydration error when using dark mode

--- components/ThemeToggler.tsx
+++ components/ThemeToggler.tsx
@@ -1,11 +1,20 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { ActionIcon, useMantineColorScheme } from "@mantine/core";
 import { IconSun, IconMoonStars } from "@tabler/icons-react";
 
-
 export default function ThemeToggler() {
   const { colorScheme, setColorScheme } = useMantineColorScheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  /** Prevent server-side rendering for first render */
+  if (!mounted) return null;
+
   const isDark = colorScheme === "dark";
 
   return (
