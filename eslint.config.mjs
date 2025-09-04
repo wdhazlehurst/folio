06be08e5dafdc4591260fa30a7e63eeef106765c
Will Hazlehurst <will@Wills-MacBook.local>
2025-09-03 23:36:55 -0400

    Updated linting script

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,10 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+const eslintConfig = [{
+  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts",
+    "generated/**"
+  ]
+}, ...compat.extends("next/core-web-vitals", "next/typescript")];
 
 export default eslintConfig;
